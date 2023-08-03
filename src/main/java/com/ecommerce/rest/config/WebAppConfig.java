bf1751034205ae15cd451747bad7a2976430db1c
Harinder <harrythind41@gmail.com>
2023-08-02 22:10:27 -0400

    feat: adding config

--- src/main/java/com/ecommerce/rest/config/WebAppConfig.java
+++ src/main/java/com/ecommerce/rest/config/WebAppConfig.java
@@ -2,18 +2,17 @@ package com.ecommerce.rest.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebAppConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
 
